18fb4e7bfdd4fc4c954ab8bdc28e7b3de23e7340
jaya sinha <jaya.sinha00@gmail.com>
2026-05-02 19:27:14 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] charsCount = new int[26];
+        for(int i=0;i<s.length();i++){
+            charsCount[s.charAt(i) - 'a']++;
+            charsCount[t.charAt(i) - 'a']--;
+        }
+
+        for(int chars: charsCount){
+            if(chars!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
